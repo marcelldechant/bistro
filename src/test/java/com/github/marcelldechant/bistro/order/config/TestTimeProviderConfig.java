2923978665edd509a5cb6cd049f2c18219821b4d
Marcell Dechant <marcell.dechant@proton.me>
2025-07-28 00:27:31 +0200

    refactor: Add time provider & added test cases for happy hour & refactor non happy hour tests

--- src/test/java/com/github/marcelldechant/bistro/order/config/TestTimeProviderConfig.java
+++ src/test/java/com/github/marcelldechant/bistro/order/config/TestTimeProviderConfig.java
@@ -4,10 +4,12 @@ import com.github.marcelldechant.bistro.order.util.TimeProvider;
 import org.mockito.Mockito;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Primary;
 
 @TestConfiguration
 public class TestTimeProviderConfig {
     @Bean
+    @Primary
     public TimeProvider timeProvider() {
         return Mockito.mock(TimeProvider.class);
     }
